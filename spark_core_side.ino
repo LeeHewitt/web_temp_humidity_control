ca384cf6bb7ea47658807d5a999e2ea38ba045d7
wildhans1 <wildhans1@yahoo.com>
2014-07-07 16:02:27 -0500

    Create spark_core_side.ino

---
+++ spark_core_side.ino
@@ -0,0 +1,91 @@
+double RH, T_C;
+int H_dat, T_dat;
+byte   _status;
+int heatpin = D4;
+int waterpin = D7;
+int tempsetting = 70;
+int rhsetting = 30;
+int tempreceived;
+int rhreceived;
+unsigned long LastUpTime = 0;
+unsigned long lastwatertime = 0;
+
+void setup() {
+    pinMode(heatpin, OUTPUT);
+    pinMode(waterpin, OUTPUT);
+    Spark.variable("rhreading", &RH, DOUBLE);
+    Spark.variable("tempreading", &T_C, DOUBLE);
+    Spark.variable("tempreceived", &tempreceived, INT);
+    Spark.variable("rhreceived", &rhreceived, INT);
+    Spark.function("tempsetting", setTemp);
+    Spark.function("rhsetting", setRH);
+    Wire.begin();
+    digitalWrite(waterpin,HIGH);
+    delay(500);
+    digitalWrite(waterpin,LOW);
+    delay(500);
+    digitalWrite(waterpin,HIGH);
+    delay(500);
+    digitalWrite(waterpin,LOW);
+    delay(3000);
+
+}
+
+void loop() {
+    
+    _status = fetch_humidity_temperature(&H_dat, &T_dat);
+    RH = (float) H_dat * 6.10e-3;
+    T_C = (float) T_dat * 1.007e-2 - 40.0;
+    if(RH<rhsetting){
+        if (millis()-lastwatertime>10000){
+        digitalWrite(waterpin,HIGH);
+        delay(500);
+        digitalWrite(waterpin,LOW);
+        lastwatertime = millis();
+        }
+    }
+    else digitalWrite(waterpin,LOW);
+    if(T_C<tempsetting)digitalWrite(heatpin,HIGH);
+    else digitalWrite(heatpin,LOW);
+    tempreceived = tempsetting;
+    rhreceived = rhsetting;
+   delay(1000);
+}
+
+
+byte fetch_humidity_temperature(int *p_H_dat, int *p_T_dat)
+{
+  byte address, Hum_H, Hum_L, Temp_H, Temp_L, _status;
+  int H_dat, T_dat;
+  address = 0x27;
+  ;
+  Wire.beginTransmission(address); 
+  Wire.endTransmission();
+  delay(100);
+
+  Wire.requestFrom((int)address, (int) 4);
+  Hum_H = Wire.read();
+  Hum_L = Wire.read();
+  Temp_H = Wire.read();
+  Temp_L = Wire.read();
+  Wire.endTransmission();
+
+  _status = (Hum_H >> 6) & 0x03;
+  Hum_H = Hum_H & 0x3f;
+  H_dat = (((int)Hum_H) << 8) | Hum_L;
+  T_dat = (((int)Temp_H) << 8) | Temp_L;
+  T_dat = T_dat / 4;
+  *p_H_dat = H_dat;
+  *p_T_dat = T_dat;
+  return(_status);
+}
+
+int setTemp(String value){
+    tempsetting = value.toInt();
+    return 0;
+}
+
+int setRH(String value){
+    rhsetting = value.toInt();
+    return 0;
+}
